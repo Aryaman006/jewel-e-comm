ad852b61f7c721f76977dfd615e8a29a58ffb928
Aryaman006 <151651851+Aryaman006@users.noreply.github.com>
2024-06-21 10:28:59 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const corsOptions = {
-  origin: "https://client-pi-ten.vercel.app/",
+  origin: "https://client-pi-ten.vercel.app",
   methods: "GET,POST,PUT,DELETE,PATCH,HEAD",
   credentials: true,
 };
